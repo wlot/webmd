ea210b151a21832cfa33b4874aa91f4f72ce6c74
Andreas Schmidt <mail@schmidt-andreas.de>
2018-02-02 22:03:25 +0100

    css: php: improve menu
    Signed-off-by: Andreas Schmidt <mail@schmidt-andreas.de>

--- scripts/Html.php
+++ scripts/Html.php
@@ -28,38 +28,51 @@ class Html
   {
     $md = new Markdown();
     $md_files = array();
+    $is_meta_file = false;
 
-    if ($dir_handle = opendir($this->git_repo_path."/".$subdir)) {
+    if (empty($subdir)) {
+      $menu = '<ul class="menu">';
+    }
+
+    if ($dir_handle = opendir($this->git_repo_path.'/'.$subdir)) {
       while (false !== ($entry = readdir($dir_handle))) {
         if (preg_match('/^.*\.md$/', $entry)) {
-          if ($entry !== "index.md") {
-            if (!empty($subdir)) {
-              array_push($md_files, $subdir."/".$entry);
-	    } else {
-	      array_push($md_files, $entry);
-	    }
-	  }
-	} else if (is_dir($this->git_repo_path."/".$subdir."/".$entry)) {
-	  if (!preg_match('/^\..*/', $entry) && $entry !== "imgs" && $entry !== "data") {
-	    $this->get_menu($entry, $menu);
-	  }
-	}
+          if (!empty($subdir)) {
+            array_push($md_files, $subdir.'/'.$entry);
+          } else {
+            array_push($md_files, $entry);
+          }
+        } else if ($entry == "dir.meta") {
+          $is_meta_file = true;
+        } else if (is_dir($this->git_repo_path.'/'.$subdir.'/'.$entry)) {
+          if (!preg_match('/^\..*/', $entry) && $entry !== "imgs" && $entry !== "data") {
+            $this->get_menu($entry, $menu);
+          }
+        }
       }
       closedir($dir_handle);
     }
 
     asort($md_files);
 
-    $menu .= '<ul class="menu">';
-    $menu .= '<h3>'.$subdir.'</h3>';
-    $menu .= '<li><a href="'.$this->http_path.'">Home</a></li>';
+    if ($is_meta_file && count($md_files) > 0) {
+      $meta = parse_ini_file($this->git_repo_path.'/'.$subdir.'/dir.meta');
 
-    foreach ($md_files as &$md_file) {
-      $title = $md->get_file_title($this->git_repo_path."/".$md_file);
-      $menu .= '<li><a href="'.$this->http_git_path.'/'.$md_file.'">'.$title.'</a></li>';
+      $menu .= '<li class="dropdown"><div class="dropbtn">'.$meta["name"].'</div><div class="submenu">';
+
+      foreach($md_files as &$md_file) {
+        $title = $md->get_file_title($this->git_repo_path.'/'.$md_file);
+        $menu .= '<a href="'.$this->http_git_path.'/'.$md_file.'">'.$title.'</a>';
+      }
+
+      $menu .= '</div></li>';
     }
 
-    $menu .= '</ul>';
+    if (empty($subdir)) {
+      $menu .= '</ul>';
+    }
   }
 }
+// vim: tabstop=2 shiftwidth=2 expandtab number
 ?>
+
