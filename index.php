4dfc8484533e890dc176e57f4a5fcd91861c388d
www-data <www-data@PC70033.wago.local>
2018-02-05 11:30:06 +0100

    php: index: replace fixed paht with global variable

--- index.php
+++ index.php
@@ -28,13 +28,13 @@ $body_attr = 'class="dotted"';
 $body = '
 <div id="page">
 <h1>White Papers und Beschreibungen</h1>
-<img id="logo" src="imgs/logo.png" />
+<img id="logo" src="'.$http_path.'/imgs/logo.png" />
 <div id="action">
   <form id="search_form" action="" method="POST" enctype="multipart/form-data">
     <input id="search_text" type="text" name="search_text" />
     <input id="search_submit" type="submit" value="Suchen" />
   </form>
-  <form id="test_form" action="/wiki/test.php" >
+  <form id="test_form" action="'.$http_path.'/test.php" >
     <input id="test_submit" type="submit" value="Test" />
   </form>
 </div>';
